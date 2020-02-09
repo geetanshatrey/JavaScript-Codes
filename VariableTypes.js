b66b9bc89dccae57d87eefee0c583931f2fecaed
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-02-10 02:54:00 +0530

    Variables in JS
    Basic code to show variables in JavaScript.

---
+++ VariableTypes.js
@@ -0,0 +1,10 @@
+// 7 types of datatype in JavaScript.
+
+var myName = "Geetansh" 
+//Can be used all throughout the code.
+myName=8.5
+
+let ourName= "Geetansh Atrey"
+//let can only be used within its scope.
+
+const pi=3.14
---
+++ Variables.js
@@ -0,0 +1,21 @@
+var a; 
+//Semi-colons can be skipped. But many people still use it to avoid confusion.
+var b = 2;
+//Assigning Variable i.e. variable b
+a = 9;
+console.log(a);
+
+var c;
+//undefined variable
+var d = "I am good!";
+var D;
+console.log(d);
+console.log(D); //variables are case sensitive
+b = a + 5;
+console.log(b);
+
+var decNo = 0.566; //Decimal Numbers in JS
+var decNo2 = 14;
+var remainder;
+remainder = decNo2 % 3;
+console.log(remainder);

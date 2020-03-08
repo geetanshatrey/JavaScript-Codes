5e6b476bbcbcde9f240ab05ae9f3e740379155c4
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-03-09 00:35:15 +0530

    Integer Conversion
    Use of parseInt() function.

---
+++ IntegerConversion.js
@@ -0,0 +1,10 @@
+function toInteger(str){
+    return parseInt(str);
+}
+
+function convertToInteger(str){
+    return parseInt(str, 2);
+}
+
+console.log(toInteger("56"));
+console.log(convertToInteger("1011"));

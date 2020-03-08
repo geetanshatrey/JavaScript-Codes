8eb0c609ce0b96418c99c1392b13326b66772d6a
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-03-09 00:35:42 +0530

    Objects in JS
    Example of use of objects in JS.

---
+++ Objects.js
@@ -0,0 +1,42 @@
+var bestCompany= {
+    "name": "Alphabet",
+    "ceo": "Sundar Pichai",
+    "rating": 4.8
+};
+var companies = 
+[
+    {
+    "name": "Google",
+    "ceo": "Sundar Pichai",
+    "noOfEmployees": 150000,
+    "is Programmer":  true
+    },
+
+    {
+        "name": "Apple",
+        "ceo": "Tim Cook",
+        "noOfEmployees": 40000,
+        "is Programmer":  true
+    },
+
+    {
+        "name": "Facebook",
+        "ceo": "Mark Zuckerberg",
+        "noOfEmployees": 80000,
+        "is Programmer":  true
+    },
+
+    {
+        "name": "AT&T",
+        "ceo": "Randall Stephenson",
+        "noOfEmployees": 10000,
+        "is Programmer":  false
+    }
+];
+
+var prog1 = companies[0]["is Programmer"];
+var prog2 = companies[1]['is Programmer'];
+console.log(prog1);
+console.log(prog2);
+console.log(bestCompany.name);
+console.log(companies[2].ceo);
